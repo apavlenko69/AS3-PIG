ca3854dea3a04d0934dcb059929a1f62f20cd343
apavlenko69 <apavlenko69@gmail.com>
2019-01-10 14:44:31 +0200

    Clean up bucket when deleting stack; minor refactoring

--- app/gittos3copy.py
+++ app/gittos3copy.py
@@ -10,10 +10,9 @@ import urllib.request
 from urllib.parse import urlparse
 
 AWS_REGION = ''  # Specific value to be assigned in Lambda handler from Event parameter-object
-s3_client = boto3.client('s3', region_name=AWS_REGION)
 
 
-def save_to_local(url):
+def get_path_and_key(url):
     branch = 'master'  # Name of Git branch as part of URL path of raw file link
 
     url_path = urlparse(url).path
@@ -26,14 +25,31 @@ def save_to_local(url):
     for f in full_path_list[::-1]:
         s3_file_key += "/" + f
 
-    urllib.request.urlretrieve(url, local_file_path)
     return local_file_path, s3_file_key[1:]
 
 
 def copy_to_s3(url, bucket):
-    loc_file_path, s3_key = save_to_local(url)
-    with open(loc_file_path, 'rb') as content:  # Upload to S3 bucket
-        s3_client.upload_fileobj(content, bucket, s3_key)
+    s3_client = boto3.client('s3', region_name=AWS_REGION)
+
+    loc_file_path, s3_key = get_path_and_key(url)
+    urllib.request.urlretrieve(url, loc_file_path)
+
+    # with open(loc_file_path, 'rb') as content:  # Binary upload to S3 bucket
+    #    s3_client.upload_fileobj(content, bucket, s3_key)
+    s3_client.upload_file(loc_file_path, bucket, s3_key)  # File upload to S3
+
+
+def clean_bucket(bucket):
+
+    s3_client = boto3.client('s3', region_name=AWS_REGION)
+    objects_to_delete = s3_client.list_objects_v2(Bucket=bucket)
+
+    delete_keys = {'Objects': []}
+    delete_keys['Objects'] = [{'Key': k} for k in [obj['Key'] for obj in objects_to_delete.get('Contents', [])]]
+
+    delete_response = s3_client.delete_objects(Bucket=bucket, Delete=delete_keys)
+
+    return delete_response
 
 
 def lambda_handler(event, context):
@@ -42,15 +58,14 @@ def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
 
     global AWS_REGION
+    AWS_REGION = event['ResourceProperties']['awsRegion']
+    print("AWS Region is " + AWS_REGION)
 
-    AWS_REGION = event['ResourceProperties']['Region']
+    properties = event['ResourceProperties']  # get the properties set in the CloudFormation resource
+    urls = properties['SourceUrls']
+    bucket = properties['TargetS3BucketName']
 
     if event['RequestType'] == 'Create':
-
-        properties = event['ResourceProperties']  # get the properties set in the CloudFormation resource
-        urls = properties['SourceUrls']
-        bucket = properties['TargetS3BucketName']
-
         try:
             for url in urls:
                 copy_to_s3(url, bucket)
@@ -61,6 +76,13 @@ def lambda_handler(event, context):
             return
 
     elif event['RequestType'] == 'Delete':
-        pass  # Add cleanup procedure
+        # Bucket cleanup procedure
+        try:
+            clean_bucket(bucket)
+
+        except Exception as e:
+            print(e)
+            cfnresponse.send(event, context, cfnresponse.FAILED, {'Response': 'Failure'})
+            return
 
     cfnresponse.send(event, context, cfnresponse.SUCCESS, {'Response': 'Success'})
